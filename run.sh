db722d68dd64b51f1ef1d01d9cb7de92c7329943
kabu <kabukunz@gmail.com>
2026-01-31 19:08:24 +0100

    some shell files

---
+++ run.sh
@@ -0,0 +1,17 @@
+python3 pdf_erase.py
+
+python3 pdf_hide.py hide \
+  --source_pdf_dir source_pdf_dir \
+  --restore_pdf_dir restore_pdf_dir \
+  --max_carriers 50 \
+  --carrier_size_max_incr 0.30
+
+python3 pdf_sync.py
+
+python3 pdf_hide.py restore
+
+python3 pdf_hide.py diff
+
+python3 pdf_hide.py hash
+
+python3 pdf_sync.py --audit
